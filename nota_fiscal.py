ea198549ba092bdb0e0f8d78d76754004588a3c7
RAYDAN GASPAR <raydangaspar@gmail.com>
2020-05-17 10:06:02 -0300

    Added nota_fiscal.py

---
+++ nota_fiscal.py
@@ -0,0 +1,46 @@
+from datetime import date
+
+
+class Item:
+    def __init__(self, descricao, valor):
+        self.__descricao = descricao
+        self.__valor = valor
+
+    @property
+    def descricao(self):
+        return self.__descricao
+
+    @property
+    def valor(self):
+        return self.__valor
+
+
+class NotaFiscal:
+    def __init__(self, razao_social, cnpj, itens, data_de_emissao, detalhes):
+        self.__razao_social = razao_social
+        self.__cnpj = cnpj
+        self.__data_de_emissao = data_de_emissao
+        if len(detalhes) > 20:
+            raise Exception('Detalhes da nota não pode ter mais do que 20 caracteres')
+        self.__detalhes = detalhes
+        self.__itens = itens
+
+    @property
+    def razao_social(self):
+        return self.__razao_social
+
+    @property
+    def cnpj(self):
+        return self.__cnpj
+
+    @property
+    def data_de_emissao(self):
+        return self.__data_de_emissao
+
+    @property
+    def detalhes(self):
+        return self.__detalhes
+
+    @property
+    def itens(self):
+        return self.__itens
