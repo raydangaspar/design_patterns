fdd1fd6cb8d36a4c2254d90781e1986b70752efb
RAYDAN GASPAR <raydangaspar@gmail.com>
2020-05-19 10:41:43 -0300

    Observer

--- nota_fiscal.py
+++ nota_fiscal.py
@@ -15,12 +15,8 @@ class Item:
         return self.__valor
 
 
-# para os parâmetros opcionais, nesse caso data_de_emissao e detalhes,
-# temos de passar um valor default no construtor para na chamada da classe
-# ser aceito não ser passado esses parâmetros
-# parâmetros opcionais devem ser passados como os últimos parâmetros
 class NotaFiscal:
-    def __init__(self, razao_social, cnpj, itens, data_de_emissao=date.today(), detalhes=''):
+    def __init__(self, razao_social, cnpj, itens, data_de_emissao=date.today(), detalhes='', observadores=[]):
         self.__razao_social = razao_social
         self.__cnpj = cnpj
         self.__data_de_emissao = data_de_emissao
@@ -28,6 +24,9 @@ class NotaFiscal:
             raise Exception('Detalhes da nota não pode ter mais do que 20 caracteres')
         self.__detalhes = detalhes
         self.__itens = itens
+
+        for observador in observadores:
+            observador(self)
 
     @property
     def razao_social(self):
@@ -48,6 +47,7 @@ class NotaFiscal:
 
 if __name__ == '__main__':
     from criador_de_nota_fiscal import CriadorDeNotaFiscal
+    from observadores import imprime, envia_por_email, salva_no_banco
 
     itens = [
         Item(
@@ -60,20 +60,11 @@ if __name__ == '__main__':
         )
     ]
 
-    # para evitar que os argumentos possam ser passados fora de ordem,
-    # podemos usar parâmetros nomeados
-    # posso inverter a ordem com parâmetros nomeados
     nota_fiscal = NotaFiscal(
         razao_social='FHSA Limitada',
         cnpj='012345678901234',
         itens=itens,
         data_de_emissao=date.today(),
-        detalhes=''
-    )
-
-    nota_fiscal_criada_com_builder = (
-        CriadorDeNotaFiscal().
-        com_razao_social('FHSA Limitada').
-        com_cnpj('012345678901234').
-        com_itens(itens).constroi()
+        detalhes='',
+        observadores=[imprime, envia_por_email, salva_no_banco]
     )
